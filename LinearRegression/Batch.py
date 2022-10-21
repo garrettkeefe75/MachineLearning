aea9f6e81b34f1fe5e7998712198c7f27e571a4d
garrettkeefe75 <57542053+garrettkeefe75@users.noreply.github.com>
2022-10-21 01:04:13 -0600

    Implemented Batch Gradient Descent

---
+++ LinearRegression/Batch.py
@@ -0,0 +1,52 @@
+from numpy.linalg import norm
+import numpy as np
+
+def costFunc(weights, data):
+    sum = 0
+    for example in data:
+        y = example[len(example)-1]
+        subset = np.array(example[:7])
+        thing = y - np.dot(np.transpose(weights), subset)
+        sum += thing ** 2
+    return sum/2
+
+trainData = []
+
+with open('./concrete/train.csv', 'r') as f:
+    for line in f:
+        listToAdd = []
+        terms = line.strip().split(',')
+        for i in range(8):
+            listToAdd.append(float(terms[i]))
+        trainData.append(listToAdd)
+
+testData = []
+
+with open('./concrete/test.csv', 'r') as f:
+    for line in f:
+        listToAdd = []
+        terms = line.strip().split(',')
+        for i in range(8):
+            listToAdd.append(float(terms[i]))
+        testData.append(listToAdd)
+
+weights = np.zeros(7)
+prevWeights = None
+toleranceLevel = 10e-6
+lr = 0.0125
+
+while True:
+    prevWeights = weights
+    sum = np.zeros(7)
+    for example in trainData:
+        y = example[len(example)-1]
+        subset = np.array(example[:7])
+        thing = y - np.dot(np.transpose(weights), subset)
+        sum += thing * subset
+    gradient = -sum
+    weights = weights - lr*gradient
+    #print(costFunc(weights, testData))
+
+    #if True: break
+    if norm((weights - prevWeights)) < toleranceLevel: break
+print(weights)
